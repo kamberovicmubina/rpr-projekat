5ed7430fd2e149786897fe4c80f43b45232e66b8
kamberovicmubina <mkamberovi1@etf.unsa.ba>
2019-08-27 14:43:10 +0200

    izmjena klijenta se pokreće tek na dupli klik

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -40,7 +41,31 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         clientList.setItems(companyModel.getClients());
 
-        clientList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Client>() {
+        clientList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                companyModel.setClickedClient(clientList.getSelectionModel().getSelectedItem());
+                if (mouseEvent.getClickCount() == 2) {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/client.fxml"));
+                    loader.setController(new ClientController(companyModel));
+                    Parent root = null;
+                    try {
+                        root = loader.load();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (root != null) {
+                        Stage secondaryStage = new Stage();
+                        secondaryStage.setTitle(companyModel.getClickedClient().getName());
+                        secondaryStage.setScene(new Scene(root, Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE));
+                        secondaryStage.initModality(Modality.APPLICATION_MODAL);
+                        secondaryStage.show();
+                    }
+                }
+            }
+        });
+
+        /*clientList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Client>() {
             @Override
             public void changed(ObservableValue<? extends Client> observableValue, Client client, Client t1) {
                 companyModel.setClickedClient(clientList.getSelectionModel().getSelectedItem());
@@ -60,7 +85,7 @@ public class Controller implements Initializable {
                     secondaryStage.show();
                 }
             }
-        });
+        });*/
         servicesNames.addAll(companyModel.getServices());
         servicesList.setItems(servicesNames);
 

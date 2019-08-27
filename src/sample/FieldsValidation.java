9651f36ffd9449de241992cb6e42d27740132936
kamberovicmubina <mkamberovi1@etf.unsa.ba>
2019-08-27 12:09:07 +0200

    napravljene validacije polja kod dodavanja nvoog klijenta

---
+++ src/sample/FieldsValidation.java
@@ -0,0 +1,47 @@
+package sample;
+
+import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FieldsValidation {
+    public static boolean validName(String n) {
+        for (char c : n.toCharArray())
+        {
+            if (Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return validAddress(n);
+    }
+
+    public static boolean validAddress(String n) {
+        boolean hasLetter = false;
+        for (int i = 0; i < n.length(); i++) {
+            if (((n.charAt(i) >= 'a' && n.charAt(i) <= 'z') || (n.charAt(i) >= 'A' && n.charAt(i) <= 'Z'))) {
+                hasLetter = true;
+                break;
+            }
+        }
+        return !n.trim().isEmpty() && hasLetter;
+    }
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static boolean validEMail (String email) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(email);
+        return matcher.find();
+    }
+
+    public static boolean validPhone (String n) {
+        return n.isEmpty() || n.matches("^(?=(?:0){1})(?=[0-9]{9,10}).*"); // pocinje nulom i ima 9 ili 10 cifara
+    }
+
+    public static boolean validDate (LocalDate n) {
+        LocalDate now = LocalDate.now();
+        return n.isBefore(now);
+    }
+
+
+}
--- src/sample/NewClientController.java
+++ src/sample/NewClientController.java
@@ -1,4 +1,84 @@
 package sample;
 
-public class NewClientController {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+public class NewClientController implements Initializable {
+    public TextField nameField;
+    public TextField addressField;
+    public TextField phoneField;
+    public TextField eMailField;
+    public DatePicker dateField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        nameField.textProperty().addListener((observableValue, o, n) -> {
+            if (FieldsValidation.validName(n)) {
+                nameField.getStyleClass().removeAll("fieldIncorrect");
+                nameField.getStyleClass().add("fieldCorrect");
+                // imeValidno = true;
+            } else {
+                nameField.getStyleClass().removeAll("fieldCorrect");
+                nameField.getStyleClass().add("fieldIncorrect");
+                //imeValidno = false;
+            }
+        });
+
+        addressField.textProperty().addListener((observableValue, o, n) -> {
+            if (FieldsValidation.validAddress(n)) {
+                addressField.getStyleClass().removeAll("fieldIncorrect");
+                addressField.getStyleClass().add("fieldCorrect");
+                // imeValidno = true;
+            } else {
+                addressField.getStyleClass().removeAll("fieldCorrect");
+                addressField.getStyleClass().add("fieldIncorrect");
+                //imeValidno = false;
+            }
+        });
+
+        phoneField.textProperty().addListener((observableValue, o, n) -> {
+            if (FieldsValidation.validPhone(n)) {
+                phoneField.getStyleClass().removeAll("fieldIncorrect");
+                phoneField.getStyleClass().add("fieldCorrect");
+                // imeValidno = true;
+            } else {
+                phoneField.getStyleClass().removeAll("fieldCorrect");
+                phoneField.getStyleClass().add("fieldIncorrect");
+                //imeValidno = false;
+            }
+        });
+
+        eMailField.textProperty().addListener((observableValue, o, n) -> {
+            if (FieldsValidation.validEMail(n)) {
+                eMailField.getStyleClass().removeAll("fieldIncorrect");
+                eMailField.getStyleClass().add("fieldCorrect");
+                // imeValidno = true;
+            } else {
+                eMailField.getStyleClass().removeAll("fieldCorrect");
+                eMailField.getStyleClass().add("fieldIncorrect");
+                //imeValidno = false;
+            }
+        });
+
+        dateField.valueProperty().addListener((observableValue, o, n) -> {
+            if (FieldsValidation.validDate(n)) {
+                dateField.getStyleClass().removeAll("fieldIncorrect");
+                dateField.getStyleClass().add("fieldCorrect");
+            } else {
+                dateField.getStyleClass().removeAll("fieldCorrect");
+                dateField.getStyleClass().add("fieldIncorrect");
+                //imeValidno = false;
+            }
+        });
+
+
+    }
 }
